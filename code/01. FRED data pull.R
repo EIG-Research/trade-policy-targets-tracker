b8d13214aac34463609a05d07a72b5d83b6ab22c
Jiaxin He <jasonhe233@hotmail.com>
2025-04-11 15:28:11 -0400

    Add FRED data pull and cleaning

--- code/01. FRED data pull.R
+++ code/01. FRED data pull.R
@@ -1,5 +1,6 @@
 # Project: Trump Trade Policy Targets Dashboard
 # File description: FRED data pull for:
+#   0. CPI-U
 #   1. Federal budget balance
 #   2. Total private construction spending, manufacturing
 #   3. Employment, Manufacturing
@@ -13,6 +14,7 @@ rm(list = ls())
 ###########################
 ###   Load Packages     ###
 ###########################
+library(curl)
 library(tidyr)
 library(dplyr)
 library(tidycensus)
@@ -48,25 +50,68 @@ path_appdata <- file.path(path_app, "cleaned_data")
 ### Data build ###
 ##################
 
-FRED_API_KEY <- your_api_key_here
+# Insert your FRED API key here
+FRED_API_KEY <- "ac17e5b7bc9c9e7c03f3f452bcb3051e"
 api_key <- Sys.getenv("FRED_API_KEY")
+
+start_date <- "1990-01-01"
+end_date <- "2025-03-01"
+
+# Query monthly CPI-U
+CPI_id <- "CPIAUCSL"
+CPI_month <- fredo(FRED_API_KEY, CPI_id, start_date, end_date)
 
 # Query monthly federal budget balance
 budget_id <- "MTSDS133FMS"
-start_date <- "1990-01-01"
-end_date <- "2025-03-01"
-budget_month <- fredo(budget_id, start_date, end_date)
-
-# Aggregate by quarter and adjust seasonally
+budget_month <- fredo(FRED_API_KEY, budget_id, start_date, end_date)
 
 # Query private construction spending, manufacturing
+construction_id <- "PRMFGCON"
+contruction_month <- fredo(FRED_API_KEY, construction_id, start_date, end_date)
 
 # Query all employees, manufacturing
+emp_manu <- "MANEMP"
+manu_month <- fredo(FRED_API_KEY, emp_manu, start_date, end_date)
 
 # Query all employees, motor vehicles and parts
+emp_motor <- "CES3133600101"
+motor_month <- fredo(FRED_API_KEY, emp_motor, start_date, end_date)
 
 # Query all employees, private
+emp_priv <- "USPRIV"
+priv_month <- fredo(FRED_API_KEY, emp_priv, start_date, end_date)
+
+# Tabulate by quarters, seasonally adjust the unadjusted ones
+quarterly <- function(df, start_month, func, seasonal = FALSE){
+  df_ts <- df %>% select(value) %>% ts(., start = start_month, frequency = 12)
+  if(seasonal){
+    df_ts <- final(seas(df_ts))
+  }
+  df_ts %>% aggregate(., nfrequency = 4, FUN = func)
+}
+
+cpi_qt <- quarterly(CPI_month, c(1990,1), mean, FALSE)
+budget_qt <- quarterly(budget_month, c(1990,1), sum, TRUE)
+construction_qt <- quarterly(contruction_month, c(1993,1), sum, TRUE)
+manu_qt <- quarterly(manu_month, c(1990,1), mean, FALSE)
+motor_qt <- quarterly(motor_month, c(1990,1), mean, FALSE)
+priv_qt <- quarterly(priv_month, c(1990,1), mean, FALSE)
+
+# Calculate quarterly inflation
+cpi_basis <- fredo(FRED_API_KEY, CPI_id, "1989-10-01", "1989-12-01")
+cpi_basis <- mean(cpi_basis$value)
+cpi_inflation <- as.numeric(cpi_qt) / c(cpi_basis, as.numeric(cpi_qt)[-length(cpi_qt)]) %>%
+  ts(., start = c(1990,1), frequency = 4)
 
 # Adjust to 2017 dollars
+cpi_adj <- cpi_qt / mean(cpi_qt[((2017-1990)*4 + 1):((2017-1990)*4 + 4)])
+budget_real <- budget_qt / cpi_adj
+construction_real <- construction_qt / cpi_adj
 
 # Calculate manufacturing and automotive shares of private employment
+manu_share <- manu_qt / priv_qt
+motor_share <- motor_qt / priv_qt
+
+# Export data
+save(cpi_inflation, budget_real, construction_real,
+     manu_qt, motor_qt, manu_share, motor_share, file = file.path(path_appdata, "fred_data.RData"))
