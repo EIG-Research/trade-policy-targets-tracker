6af1af4dab944db9e894862f3d1289e1c9f34b0e
Jiaxin He <jasonhe233@hotmail.com>
2025-04-14 12:29:19 -0400

    add basic page structure

--- trade-target-tracker/app.R
+++ trade-target-tracker/app.R
@@ -32,53 +32,23 @@ rsconnect::setAccountInfo(name='economicinnovationgroup',
 #################
 ### Load Data ###
 #################
-# Query state fips-abbreviation crosswalk
-state_codes <- fips_codes %>% select(state_code, state) %>%
-  distinct(state_code, .keep_all = TRUE) %>%
-  mutate(state_code = as.numeric((state_code))) %>%
-  filter(state_code <= 56)
+# Define user-specific project directories
+project_directories <- list(
+  "name" = "PATH TO GITHUB REPO",
+  "jiaxinhe" = "/Users/jiaxinhe/Documents/projects/trade-policy-targets-tracker"
+)
 
-# Load in cleaned data
-# Change the path to "../data_cleaned" if running the app locally
-# DO NOT USE SETWD IN CODE IF DEPLOYING TO SHINYAPPS.IO
-hist_occ_data <- read.csv(file.path("data_cleaned", "hist_occ_data.csv")) %>% select(-X)
-occ_income_summary <- read.csv(file.path("data_cleaned", "occ_income_summary.csv")) %>% select(-X)
-demo_summary <- read.csv(file.path("data_cleaned", "demo_summary.csv")) %>% select(-X)
+# Setting project path based on current user
+current_user <- Sys.info()[["user"]]
+if (!current_user %in% names(project_directories)) {
+  stop("Root folder for current user is not defined.")
+}
 
-# Fix the column names
-colnames(demo_summary) <- c("state", "income_cutoff", "F", "M", "16-24", "25-34", "35-44", "45-64", "65 and above",
-                            "African American", "Asian and Pacific Islander", "Multi-racial", 
-                            "Native American", "Other race", "White", "Bachelor's or above",
-                            "High school or below", "Some college", "quantile")
+path_project <- project_directories[[current_user]]
+path_app <- file.path(path_project, "trade-target-tracker")
+path_appdata <- file.path(path_app, "cleaned_data")
 
-# Make median incomes integers
-occ_income_summary <- occ_income_summary %>% mutate(median_income = as.integer(median_income))
-
-# Ordered list of occupations
-occ_list <- c("Chief Executives",
-              "Other Managers",
-              "Business Operations Specialists",
-              "Financial Specialists",
-              "Computer and Mathematical",
-              "Architecture and Engineering",
-              "Life, Physical, and Social Science", 
-              "Community and Social Services",
-              "Legal", "Education, Training, and Library",
-              "Arts, Design, Entertainment, Sports, and Media",
-              "Healthcare Practitioners and Technicians",
-              "Healthcare Support", "Protective Service",
-              "Food Preparation and Serving",
-              "Building and Grounds Cleaning and Maintenance",
-              "Personal Care and Service",
-              "Sales and Related",
-              "Office and Administrative Support",
-              "Farming, Fishing, and Forestry",
-              "Construction",
-              "Extraction",
-              "Installation, Maintenance, and Repair",
-              "Production",
-              "Transportation and Material Moving",
-              "Not Identified")
+load(file.path(path_appdata, "fred_data.RData"))
 
 ######################
 ### Build Shiny UI ###
@@ -98,53 +68,17 @@ eig_palette <- function(n_colors, input_palette){
   }
 }
 
-ui <- page_sidebar(
-  tags$style(type='text/css', "#description { font-size: 13px; }
-             .selectize-input { font-size: 75%; }
-             .selectize-dropdown { font-size: 75%; }
-             .income_percent { display: flex; justify-content: center; margin: auto; width: 80%; }
-             .irs-from, .irs-to, .irs-min, .irs-max { visibility: hidden !important"),
-  
-  title = "State Occupation Breakdown by Income Threshold",
-  verbatimTextOutput("description"),
-  sidebar = sidebar(
-    style = "font-size:75%;",
-    
-    ### U.S. State Selector ###
-    selectizeInput( 
-      "state_selector", 
-      "Select state to view", 
-      choices = state_codes$state,
-      selected = "NY"
-    ),
-    
-    ### Occupation Category Selector ###
-    selectizeInput(
-      "occupation_selector", 
-      "Select occupations", 
-      choices = occ_list,
-      selected = c(
-        "Chief Executives",
-        "Other Managers"
-      ),
-      multiple = TRUE
-    ),
-    
-    ### Income percentile selector ###
-    sliderInput("income_percent", label = NULL, min = 0, max = 1, value = 0.95, step = 0.025),
-    
-    width = validateCssUnit(250)
-  ),
+ui <- page_fillable(
   
   navset_card_tab(
-    ### Plot with Income Percentile Slider ###
-    nav_panel("Income Distribution", plotOutput("plot_dist")),
+    ### Plot 1 ###
+    nav_panel("Plot 1", plotOutput("plot_1")),
     
-    ### Median Incomes per Category above Wage Cap ###
-    nav_panel("Median Incomes", tableOutput("tbl_inc")),
+    ### Plot 2 ###
+    nav_panel("Plot 2", plotOutput("plot_2")),
     
-    ### Demographic Characteristics of Those above Wage Cap ###
-    nav_panel("Demographics", plotOutput("plots_dem"))
+    ### Plot 3 ###
+    nav_panel("Plot 3", plotOutput("plot_3"))
   )
 )
 
@@ -153,118 +87,17 @@ ui <- page_sidebar(
 ##########################
 
 server <- function(input, output) {
-  # Filter by the state selected by the user and condense occupation categories
-  # to only those selected by the user
-  selected_occ_data <- reactive({
-    hist_occ_data %>% filter(state == input$state_selector) %>%
-      mutate(Occupation = case_when(occ_category %in% input$occupation_selector ~ occ_category,
-                                    TRUE ~ "Other Occupations")) %>%
-      mutate(Occupation = factor(Occupation, ordered = TRUE,
-                                 levels = c(input$occupation_selector, "Other Occupations")))
-  })
-  
-  # Filter by state, quantile, and occupations selected
-  excluded_occ_income <- reactive({
-    occ_income_summary %>% ungroup() %>%
-      filter(state == input$state_selector,
-             occ_category %in% input$occupation_selector,
-             quantile == as.integer(as.numeric(input$income_percent)*40) + 1) %>%
-      select(occ_category, median_income, share_quantile) %>%
-      mutate(share_quantile = percent(share_quantile, accuracy = 0.1)) %>%
-      rename(Occupations = occ_category,
-             "Median Income above Income Threshold" = median_income,
-             "Share of Workers above Income Threshold" = share_quantile)
-  })
-  
-  selected_demo_data <- reactive({
-    demo_summary %>% filter(state == input$state_selector,
-                            quantile == as.integer(as.numeric(input$income_percent)*40) + 1)
-  })
-  selected_income_cutoff <- reactive({
-    demo_summary %>% filter(state == input$state_selector,
-                            quantile == as.integer(as.numeric(input$income_percent)*40)) %>%
-      .$income_cutoff})
-  selected_sex <- reactive({
-    selected_demo_data() %>% select(M, `F`) %>%
-      pivot_longer(cols = everything(), names_to = "Sex", values_to = "Share") %>%
-      mutate(Sex = factor(Sex, ordered = TRUE, levels = c("F", "M")),
-        dummy = as.character(row_number()))
-  })
-  selected_age <- reactive({
-    selected_demo_data() %>% select(5:9) %>%
-      pivot_longer(cols = everything(), names_to = "Age", values_to = "Share") %>%
-      mutate(Age = factor(Age, ordered = TRUE, levels = c("65 and above", "45-64", "35-44", "25-34", "16-24")),
-             dummy = as.character(row_number()))
-  })
-  selected_race <- reactive({
-    selected_demo_data() %>% select(10:15) %>%
-      pivot_longer(cols = everything(), names_to = "Race", values_to = "Share") %>%
-      mutate(Race = factor(Race, ordered = TRUE, levels = c("Native American", "Other race", "Multi-racial",
-                                                            "Asian and Pacific Islander", "African American", "White")),
-             dummy = as.character(row_number()))
-  })
-  selected_edu <- reactive({
-    selected_demo_data() %>% select(16:18) %>%
-      pivot_longer(cols = everything(), names_to = "Education", values_to = "Share") %>%
-      mutate(Education = factor(Education, ordered = TRUE, levels = c("Bachelor's or above", "Some college",
-                                                                      "High school or below")),
-             dummy = as.character(row_number()))
-  })
-  
-  income_cutoff <- reactive({})
-  
-  output$description <- renderText(
-    "This dashboard tracks the occupational and demographic breakdown of each U.S. state above a given income threshold:
-    1. The first tab displays the distribution of selected occupations across the income distribution.
-    2. The second tab shows the median incomes and workforce share of selected occupations above the income threshold.
-    3. The third tab illustrates the sex, age, race, and education composition of all workers above the income threshold."
-  )
-  output$plot_dist <- renderPlot(
-    ggplot(selected_occ_data(), aes(fill = Occupation, x = percentile, y = share_quantile)) +
-      geom_bar(position = "fill", stat = "identity", just = 1) +
-      
-      geom_vline(xintercept = as.numeric(input$income_percent), linetype="solid", size=0.8) +
-      geom_text(aes(x = as.numeric(input$income_percent) - 0.02, y = 1.05,
-                    label = paste0(as.character(as.numeric(input$income_percent) * 100), "%")),
-                stat = "unique", size = 3) +
-      geom_text(aes(x = as.numeric(input$income_percent) + 0.03, y = 1.05,
-                    label = paste0("$", label_comma(accuracy = 1)(selected_income_cutoff()))),
-                stat = "unique", size = 3) +
-      geom_rect(xmin = 0, xmax = as.numeric(input$income_percent), ymin = 0, ymax = 1,
-                fill = "grey", alpha = 0.01) +
-      
-      ylab("Share of Workers in the Income Bin") +
-      xlab("Weighted Income Percentile") + theme_bw() +
-      scale_fill_manual(values = eig_palette((length(input$occupation_selector) + 1), eig_colors))
+  output$plot_1 <- renderPlot(
+    ggplot()
   )
   
-  output$tbl_inc <- renderTable(
-    excluded_occ_income()
+  output$plot_2 <- renderPlot(
+    ggplot()
   )
   
-  output$plots_dem <- renderPlot({
-    plot_sex <- ggplot(selected_sex(), aes(x = Share, y = Sex, fill = dummy)) +
-      geom_bar(stat = "identity") + geom_text(aes(label = percent(Share, accuracy = 0.1)), hjust = -0.02) +
-      theme_bw() + scale_fill_manual(values = eig_palette(nrow(selected_sex()), eig_colors)) +
-      theme(legend.position = "none") + xlim(0,1)
-    
-    plot_age <- ggplot(selected_age(), aes(x = Share, y = Age, fill = dummy)) +
-      geom_bar(stat = "identity") + geom_text(aes(label = percent(Share, accuracy = 0.1)), hjust = -0.02) +
-      theme_bw() + scale_fill_manual(values = eig_palette(nrow(selected_age()), eig_colors)) +
-      theme(legend.position = "none") + xlim(0,1)
-    
-    plot_race <- ggplot(selected_race(), aes(x = Share, y = Race, fill = dummy)) +
-      geom_bar(stat = "identity") + geom_text(aes(label = percent(Share, accuracy = 0.1)), hjust = -0.02) +
-      theme_bw() + scale_fill_manual(values = eig_palette(nrow(selected_race()), eig_colors)) +
-      theme(legend.position = "none") + xlim(0,1)
-    
-    plot_edu <- ggplot(selected_edu(), aes(x = Share, y = Education, fill = dummy)) +
-      geom_bar(stat = "identity") + geom_text(aes(label = percent(Share, accuracy = 0.1)), hjust = -0.02) +
-      theme_bw() + scale_fill_manual(values = eig_palette(nrow(selected_edu()), eig_colors)) +
-      theme(legend.position = "none") + xlim(0,1)
-    
-    plot_grid(plot_sex, plot_age, plot_race, plot_edu, ncol = 1, align = "v", axis = "lr")
-  })
+  output$plot_3 <- renderPlot(
+    ggplot()
+  )
 }
 
 shinyApp(ui = ui, server = server)
